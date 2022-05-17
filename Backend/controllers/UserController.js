b70a256840820e1785a96e341145797c209b8561
uranghishna <hishna.di@gmail.com>
2022-05-18 01:22:06 +0700

    frontend

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -31,6 +31,28 @@ export const createUser = async(req, res) => {
     }
 }
 
+export const updateUser = async(req, res) => {
+    try {
+        await User.update(req.body,{
+            where: {
+                id: req.params.id
+        }
+    });
+        res.status(200).json({msg: "User Updated"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
-
-
+export const deleteUser = async(req, res) => {
+    try {
+        await User.destroy({
+            where: {
+                id: req.params.id
+        }
+    });
+        res.status(200).json({msg: "User Deleted"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
